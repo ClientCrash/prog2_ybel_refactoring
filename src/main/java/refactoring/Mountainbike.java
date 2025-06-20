7a03860b38b5e0ba5df409a7f75ac1e25cad6321
Client <client@clientcrash.com>
2025-06-20 02:05:57 +0200

    fix custom mountainbike pricing

--- src/main/java/refactoring/Mountainbike.java
+++ src/main/java/refactoring/Mountainbike.java
@@ -2,6 +2,7 @@ package refactoring;
 
 /**
  * Represents a mountain bike with gear system and maximum speed.
+ * Features bulk discount for 3+ bikes (10% off).
  */
 public class Mountainbike extends Bike {
 
@@ -19,5 +20,20 @@ public class Mountainbike extends Bike {
     @Override
     public Integer getBatteryCapacity() {
         return null;
+    }
+
+    /**
+     * Calculates base price for mountain bikes.
+     * Bulk discount: 10% off for 3+ bikes.
+     * @param purchaseAmount the number of bikes to purchase
+     * @return the base price with mountain bike discounts
+     */
+    @Override
+    protected double calculateBasePrice(int purchaseAmount) {
+        if (purchaseAmount > 2) {
+            return purchaseAmount * getPrice() * 0.9;
+        } else {
+            return getPrice() * purchaseAmount;
+        }
     }
 }
